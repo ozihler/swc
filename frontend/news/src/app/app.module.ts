0890574d194c8133bacadfb96c9d89a59d0f840c
Oliver Zihler <ozihler@gmail.com>
2020-09-11 15:51:51 +0200

    introduce ngrx

--- frontend/news/src/app/app.module.ts
+++ frontend/news/src/app/app.module.ts
@@ -6,6 +6,10 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {HelloWorldModule} from "./hello-world/hello-world.module";
 import {WeatherModule} from "./weather/weather.module";
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import {WeatherModule} from "./weather/weather.module";
         AppRoutingModule,
         HelloWorldModule,
         HttpClientModule,
-        WeatherModule
+        WeatherModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        EffectsModule.forRoot([])
     ],
   providers: [],
   bootstrap: [AppComponent]
