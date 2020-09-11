ffcefa26df553e12cbb196d18e2b4fc2fd6337cd
Oliver Zihler <ozihler@gmail.com>
2020-09-11 09:33:59 +0200

    initialise project

---
+++ backend/src/test/java/com/example/swc/SwcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
