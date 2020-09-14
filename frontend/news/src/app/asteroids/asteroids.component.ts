62322b285b0a810d5764bf37a4dd8be81a9e88d3
Oliver Zihler <ozihler@gmail.com>
2020-09-14 15:13:38 +0200

    remove unused faBomb

--- frontend/news/src/app/asteroids/asteroids.component.ts
+++ frontend/news/src/app/asteroids/asteroids.component.ts
@@ -4,7 +4,6 @@ import {select, Store} from "@ngrx/store";
 import {getAsteroids, isLoading} from "./selectors/asteroids.selectors";
 import {Observable} from "rxjs";
 import {loadAsteroids} from "./actions/asteroids.actions";
-import {faBomb} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'nw-asteroids',
@@ -12,11 +11,11 @@ import {faBomb} from "@fortawesome/free-solid-svg-icons";
     <div *ngIf="isLoading$ | async">Lade Asteroidendaten</div>
     <div *ngIf="!(isLoading$ | async)">
       <div class="card-columns">
-          <nw-asteroid-info
-            *ngFor="let asteroid of asteroids$ | async"
-            [asteroid]="asteroid">
+        <nw-asteroid-info
+          *ngFor="let asteroid of asteroids$ | async"
+          [asteroid]="asteroid">
 
-          </nw-asteroid-info>
+        </nw-asteroid-info>
       </div>
     </div>
   `,
@@ -25,12 +24,10 @@ import {faBomb} from "@fortawesome/free-solid-svg-icons";
 export class AsteroidsComponent implements OnInit {
   asteroids$: Observable<Asteroid[]>;
   isLoading$: Observable<boolean>;
-  faBomb: any;
 
   constructor(private store: Store<State>) {
     this.asteroids$ = store.pipe(select(getAsteroids));
     this.isLoading$ = store.pipe(select(isLoading));
-    this.faBomb = faBomb;
   }
 
   ngOnInit(): void {
