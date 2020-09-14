6d70dfbd723f306117e036396f00ebba6840cb06
Oliver Zihler <ozihler@gmail.com>
2020-09-14 12:24:23 +0200

    basic card layout for asteroids

--- frontend/news/src/app/asteroids/asteroids.component.ts
+++ frontend/news/src/app/asteroids/asteroids.component.ts
@@ -10,13 +10,22 @@ import {loadAsteroids} from "./actions/asteroids.actions";
   template: `
     <div *ngIf="isLoading$ | async">Lade Asteroidendaten</div>
     <div *ngIf="!(isLoading$ | async)">
-      <div *ngFor="let asteroid of asteroids$ | async">
+      <div class="row" *ngFor="let asteroid of asteroids$ | async">
         <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{asteroid.name}} ({{asteroid.id}})</h5>
-              <p class="card-text">Average Distance: {{asteroid.averageLunarDistance}} ld
-                ({{asteroid.averageMissDistanceInKm}} km)</p>
-            </div>
+          <div class="card-body">
+            <h5 class="card-title">{{asteroid.name}} ({{asteroid.id}})</h5>
+            <p class="card-text">
+              Average Distance: {{asteroid.averageLunarDistance | number:'1.1-2'}} ld
+              ({{asteroid.averageMissDistanceInKm| number:'1.1-2'}} km)
+            </p>
+            <p class="card-text">
+              Kinetic Energy (Tons of TNT) {{asteroid.kineticEnergyInTonsOfTNT| number:'1.1-2'}} t
+              ({{asteroid.magnitude}})
+            </p>
+            <p class="card-text">
+              {{asteroid.numberOfHiroshimaBombs}} Hiroshima Bombs
+            </p>
+          </div>
         </div>
       </div>
     </div>
